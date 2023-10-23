44616411793c011c925f0361ed75480e310565dd
shar-rahman <sharjeelr00@gmail.com>
2023-10-23 12:45:00 -0700

    created manage.py

---
+++ manage.py
@@ -0,0 +1,24 @@
+# server.py: flask framework for running web-crawler app
+
+from flask import Flask
+from .crawler import crawler
+from .classifier import classifier
+
+app = Flask(_name_)
+
+# api endpoints
+# 1. ("/"): how-to guide / readme
+# 2. ("/crawler"): use crawler with search function: ?search=XXX
+# 3. ("/classify"): use classifier with image path: ?path=XXX
+
+@app.route('/')
+def readme():
+    # instructions on using api
+
+@app.route('/crawler')
+def crawl():
+    # init crawler
+
+@app.route('/classify')
+def classify():
+    # init classifier
